459b62617a0277f28096f0a6695895c400c1106a
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2022-12-12 13:00:40 -0800

    Add Bungee and Source Sans Pro fonts to header and layout.css

--- server/lib/util/user-helper.js
+++ server/lib/util/user-helper.js
@@ -32,10 +32,10 @@ module.exports = {
       Female: ["https://i.imgur.com/nlhLi3I.png","https://i.imgur.com/z5LNkkB.png","https://i.imgur.com/v0JXau2.png","https://i.imgur.com/lRUnDgU.png", "https://i.imgur.com/3GvwNBf.png"],
       Male: ["https://i.imgur.com/73hZDYK.png","https://i.imgur.com/5fUVPRP.png","https://i.imgur.com/DVpDmdR.png","https://i.imgur.com/2WZtOD6.png", "https://i.imgur.com/ilT4JDe.png"]
     
-    }
+    };
     
-    const avatarArray = avatars[gender]
-    const userAvatar = avatarArray[Math.floor(Math.random()*avatarArray.length)]
+    const avatarArray = avatars[gender];
+    const userAvatar = avatarArray[Math.floor(Math.random()*avatarArray.length)];
   
 
     return {
