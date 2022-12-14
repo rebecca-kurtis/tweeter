d9eb044526929c6942eb61ebc9322f269aa269a2
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2022-12-13 21:40:13 -0800

    Add Validation alert checks/messages

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,33 +6,6 @@
 
 $(document).ready(function() {
 
-  // Fake data taken from initial-tweets.json
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd"
-  //     },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ];
-
   //create a tweet box with contents from inputed data
   const createTweetElement = function(tweets) {
     const format = window.timeago.format;
@@ -93,8 +66,24 @@ $(document).ready(function() {
   $form.on('submit', (event) => {
     event.preventDefault();
 
+    //Validation checks
+    let maxChar = 140;
+    let $charsEntered = $('#tweet-text').val().length;
+
+    if ($charsEntered > maxChar) {
+      window.alert('Your tweet is too long!');
+      return;
+    }
+    if ($charsEntered === 0) {
+      window.alert('Your tweet is empty!');
+      return;
+    }
+
     const data = $form.serialize();
     console.log(data);
+    
+    //clear the text portion
+    $("#tweet-text").val('');
 
     $.post('/tweets', data, (response) => {
       console.log('post response', response);
